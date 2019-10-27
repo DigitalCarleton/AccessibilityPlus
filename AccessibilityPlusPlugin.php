c922a40c4bd573ed25fbe19c92d96cb27dd30636
padillac <shinimaninima@gmail.com>
2019-10-27 16:12:19 -0500

    fixed new filter function so alt-text works everywhere on the site

--- AccessibilityPlusPlugin.php
+++ AccessibilityPlusPlugin.php
@@ -38,20 +38,15 @@ class AccessibilityPlusPlugin extends Omeka_Plugin_AbstractPlugin
     //hooks into the Image Tag Attributes filter
     public function filterImageTagAttributes($attrs, $args)
     {
-      $file = $args['file'];
-
-      echo "<h1>HELLOOOOOOooooooo</h1>";
-      var_dump($attrs);
+      $item = $args['file']->getItem();
 
       //checks if the option has been set in the options table or not
       $selected_option = get_option('alt_text_data');
       if ($selected_option){
-          $newAlt = metadata($file, array('Dublin Core', $selected_option));
+          $newAlt = metadata($item, array('Dublin Core', $selected_option));
       }
       //if no option set or if the requested metadata is missing, use the image title instead
       if (!$newAlt){
-          //gets the item image and gets the requested metadata from it
-          $item = $file->getItem();
           $newAlt = metadata($item, array('Dublin Core', 'Title'));
           //if the image is untitled, set the alt-text as "Untitled Image"
           if ("$newAlt" == '[Untitled]'){
@@ -59,49 +54,10 @@ class AccessibilityPlusPlugin extends Omeka_Plugin_AbstractPlugin
           }
       }
 
-      echo "NEWALT: ";
-      var_dump($newAlt);
-
       $attrs['alt'] = $newAlt;
 
       return $attrs;
-
     }
 
-
-    /*
-    //hooks into the File Markup filter
-    public function oldFilterFunction($html, $args)
-    {
-      //Checks if the file has a thumbnail or fullsize image
-      $file = $args['file'];
-      if($file->hasThumbnail() || $file->hasFullsize()){
-        //finds the position where alt-text is defined in the HTML
-        $posStart = strpos($html, 'alt');
-        $posStop = strpos($html, 'title');
-        $length = $posStop - $posStart;
-        $newAlt = NULL;
-        $selected_option = get_option('alt_text_data');
-        //checks if the option has been set in the options table or not
-        if ($selected_option){
-            $newAlt = metadata($file, array('Dublin Core', $selected_option));
-        }
-        //if no option set or if the requested metadata is missing, use the image title instead
-        if (!$newAlt){
-            //gets the item image and gets the requested metadata from it
-            $item = $file->getItem();
-            $newAlt = metadata($item, array('Dublin Core', 'Title'));
-            //if the image is untitled, set the alt-text as "Untitled Image"
-            if ("$newAlt" == '[Untitled]'){
-                $newAlt = "Untitled Image";
-            }
-        }
-        $newCode = 'alt="'.$newAlt.'"';
-        //replaces code generating the alt-text in the HTML with $newCode
-        $html = substr_replace($html, $newCode, $posStart, $length);
-      }
-      return $html;
-    }
-    */
 
 }
