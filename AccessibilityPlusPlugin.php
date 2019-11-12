b607add61396444a2d71a8024958946931ad9fa4
padillac <shinimaninima@gmail.com>
2019-11-12 09:32:55 -0600

    initialized newAlt outside conditional

--- AccessibilityPlusPlugin.php
+++ AccessibilityPlusPlugin.php
@@ -36,9 +36,9 @@ class AccessibilityPlusPlugin extends Omeka_Plugin_AbstractPlugin
 
 
     //hooks into the Image Tag Attributes filter
-    public function filterImageTagAttributes($attrs, $args)
-    {
+    public function filterImageTagAttributes($attrs, $args) {
       $item = $args['file']->getItem();
+      $newAlt = "";
 
       //checks if the option has been set in the options table or not
       $selected_option = get_option('alt_text_data');
