0d84b5b84472de4a3f97de4fa8e8141e1a89fc8f
padillac <shinimaninima@gmail.com>
2019-11-16 14:22:52 -0600

    Re-did all descriptions on settings page, adding a top-level plugin description and specific descriptions for each setting

--- libraries/AccessibilityPlus/Form/Settings.php
+++ libraries/AccessibilityPlus/Form/Settings.php
@@ -22,6 +22,18 @@ class AccessibilityPlus_Form_Settings extends Omeka_Form
             $valueOptions = array($front => $valueOptions[$front]) + $valueOptions;
         }
 
+        //sets description for alt text selector
+        $altTextDescription = 'Alternate Text or Alt-Text is the text read by screenreaders '
+        . 'when coming across images on a website. Omeka by default uses the filename, '
+        . 'which can frequently be indecipherable (i.e. DSC00891.jpg). '
+        . 'To address this issue, this plugin lets you choose a metadata element '
+        . 'from the Dublin Core (such as "Title" or "Description") to be used as the alt-text. '
+        . 'So far this only works for when looking at fullsized images, not thumbnails. '
+        . 'The plugin <a href="https://omeka.org/classic/plugins/DublinCoreExtended/">'
+        . 'Dublin Core Extended </a> adds the full set of Dublin Core properties, '
+        . 'include alternative title for alt-text. If the element you choose is missing, '
+        . 'from an image this plugin uses the item title by default.';
+
         //builds the dropdownmenu
         $this->addElement(
             'select',
@@ -29,9 +41,17 @@ class AccessibilityPlus_Form_Settings extends Omeka_Form
             array(
                 'label' => 'Dublin Core Metadata Types',
                 'multiOptions' => $valueOptions,
+                'description' => $altTextDescription,
               )
         );
 
+
+
+        //sets description for keyboard focus checkbox
+        $keyboardFocusDescription = "Some users prefer to navigate a website by tabbing through elemements "
+        . "using their keyboard. This setting places a colorful border around the currently selected element "
+        . "when the user is using their keyboard to make it easier to see which element is currently "
+        . "highlighted and stay oriented on the page.";
 
         //builds keyboard focus outline checkbox
         $checked = get_option('keyboard_focus_outline');
@@ -40,9 +60,18 @@ class AccessibilityPlus_Form_Settings extends Omeka_Form
           'keyboard_focus_outline',
           array(
             'label' => 'Turn on Keyboard Focus Outlines',
-            'value' => $checked
+            'value' => $checked,
+            'description' => $keyboardFocusDescription,
           )
         );
+
+        //sets the description for the keyboard focus color selector
+        $keyboardFocusColorDescription = "This setting allows you to choose the color "
+        . "of the borders around highlighted elements. "
+        . "Choose a color that is easily visible and contrasts well with your background colors. "
+        . "This field accepts <a href='https://www.w3schools.com/cssref/css_colors.asp'>CSS Color Names</a> "
+        . " or six-digit <a href='https://www.google.com/search?q=color+picker'>hex codes</a> that start with '#', "
+        . "for example #0000FF, which means blue.";
 
         //builds outline color text field
         $outlineColor = get_option('outline_color');
@@ -51,7 +80,8 @@ class AccessibilityPlus_Form_Settings extends Omeka_Form
           'outline_color',
           array(
             'value' => $outlineColor,
-            'label' => 'Element Outline Color (#hexcode or CSS color name)'
+            'label' => 'Element Outline Color (#hexcode or CSS color name)',
+            'description' => $keyboardFocusColorDescription,
           )
         );
 
@@ -63,6 +93,9 @@ class AccessibilityPlus_Form_Settings extends Omeka_Form
                     'ViewHelper',
                     array('HtmlTag', array('tag' => 'div', 'class' => 'field')))),
         ));
+
+
+
 
         //makes display separate groups for visual formatting
         $this->addDisplayGroup(
